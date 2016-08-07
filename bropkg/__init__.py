3870073d029554adfbaadbb187b1ac57899b41f8
Jon Siwek <jsiwek@illinois.edu>
2016-08-07 12:05:40 -0500

    Docs: show actual version in sidebar.

--- bropkg/__init__.py
+++ bropkg/__init__.py
@@ -9,7 +9,7 @@ be added in order to help log/debug applications.
 
 import logging
 
-__version__ = "0.2-25"
+__version__ = "0.2-26"
 __all__ = ['manager', 'package', 'source']
 
 LOG = logging.getLogger(__name__)
