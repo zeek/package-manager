6950da63331880788e893677c123a89c4df4a828
Jon Siwek <jsiwek@illinois.edu>
2016-08-09 13:06:24 -0500

    Using ``install --version`` now also pins the package.

--- bropkg/__init__.py
+++ bropkg/__init__.py
@@ -9,7 +9,7 @@ be added in order to help log/debug applications.
 
 import logging
 
-__version__ = "0.2-33"
+__version__ = "0.2-34"
 __all__ = ['manager', 'package', 'source']
 
 LOG = logging.getLogger(__name__)
