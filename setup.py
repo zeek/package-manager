f229031c2a30c29ae5261ad3967d6146671969d6
Jon Siwek <jsiwek@illinois.edu>
2016-08-11 19:45:21 -0500

    setup.py packaging improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bro-pkg',
-    version=open('VERSION').read(),
+    version=open('VERSION').read().replace('-', '.dev', 1).strip(),
     description='The Bro Package Manager',
     long_description=open('README').read(),
     license='University of Illinois/NCSA Open Source License',
@@ -13,4 +13,15 @@ setup(
     scripts=['bro-pkg'],
     packages=['bropkg'],
     install_requires=['gitpython', 'semantic_version'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: Utilities',
+    ],
 )
