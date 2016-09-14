6e9b1c78f68ee28673fd54879a029c45bb51cf73
Jon Siwek <jsiwek@illinois.edu>
2016-09-14 15:18:11 -0500

    Updating CHANGES and VERSION.

--- bropkg/__init__.py
+++ bropkg/__init__.py
@@ -9,7 +9,7 @@ be added in order to help log/debug applications.
 
 import logging
 
-__version__ = "0.5-2"
+__version__ = "0.6"
 __all__ = ['manager', 'package', 'source']
 
 LOG = logging.getLogger(__name__)
