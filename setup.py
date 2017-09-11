d1b3aef693fb56f4679f0ba703f52d3daafae75a
Hilko Bengen <bengen@hilluzination.de>
2017-09-11 11:45:41 +0200

    Require extra configparser package only for Python < 3.5 where it is not included

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from sys import version_info
+
+install_requires = ['gitpython', 'semantic_version', 'btest']
+
+if version_info[0] + version_info[1] / 10 < 3.5:
+    install_requires.append("configparser")
 
 setup(
     name='bro-pkg',
@@ -12,7 +18,7 @@ setup(
     url='https://github.com/bro/package-manager',
     scripts=['bro-pkg'],
     packages=['bropkg'],
-    install_requires=['gitpython', 'semantic_version', 'configparser', 'btest'],
+    install_requires=install_requires,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
