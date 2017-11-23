836f07cb90d0bb924cc2bee8a9e7f7b7f3be3b88
Jon Siwek <jsiwek@corelight.com>
2017-11-23 13:54:11 -0600

    GH-23: another attempt to remove "Edit on GitHub" link.

--- doc/conf.py
+++ doc/conf.py
@@ -132,6 +132,10 @@ html_theme_options = {
     'display_version': True,
 }
 
+html_context = {
+    "display_github": False # The "Edit on GitHub" link
+}
+
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
