c05683b7833a685f9009fbbc136c5e7f15af76e7
Jon Siwek <jsiwek@corelight.com>
2020-12-04 12:48:14 -0800

    Updating CHANGES and VERSION.

--- zeekpkg/__init__.py
+++ zeekpkg/__init__.py
@@ -9,7 +9,7 @@ be added in order to help log/debug applications.
 
 import logging
 
-__version__ = "2.5.0"
+__version__ = "2.5.0-4"
 __all__ = ['manager', 'package', 'source']
 
 LOG = logging.getLogger(__name__)
