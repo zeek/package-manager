49f94b91093bc8decf5c8c9ffcb2ff8a93149302
Jon Siwek <jsiwek@corelight.com>
2021-05-18 15:06:32 -0700

    Add an baseline canonifier to user-mode btest

--- zeekpkg/__init__.py
+++ zeekpkg/__init__.py
@@ -9,7 +9,7 @@ may be added in order to help log/debug applications.
 
 import logging
 
-__version__ = "2.8.0-22"
+__version__ = "2.8.0-24"
 __all__ = ['manager', 'package', 'source', 'template', 'uservar']
 
 LOG = logging.getLogger(__name__)
